4c183f19f9a439f7dcaf0735e33f176fdc886c8d
baydre_africa <baydreafrica@gmail.com>
2024-04-28 10:12:16 +0100

    implement logout function to respond to DELETE /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask app
 """
-from flask import Flask, jsonify, request, abort, redirect
+from flask import Flask, jsonify, request, redirect, abort
 from auth import Auth
 
 app = Flask(__name__)
