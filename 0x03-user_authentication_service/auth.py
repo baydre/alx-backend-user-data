51fab8ccc16c0f674961e050cd0e28d932d8e742
baydre_africa <baydreafrica@gmail.com>
2024-04-28 21:09:34 +0100

    implement Auth.update_password using reset_token args and returns None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -118,3 +118,15 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        implements AUTH.update_password method using
+        reset_token & password as args and returns None.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        pwd = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
