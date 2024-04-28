b11599dcb98df82238e7e9f430f06db872e3b3fd
baydre_africa <baydreafrica@gmail.com>
2024-04-28 15:04:12 +0100

    implement profile function to respond to GET /profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,5 +69,18 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """
+    implement profile function to respond
+    to GET /profile route.
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
